2f182f96091d910e96d90834944d6339401da422
Shahd Elagamy <104149324+shahd-elagamy@users.noreply.github.com>
2025-12-28 10:35:11 +0200

    Delete planner directory

--- planner/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user_profile import UserProfile
-from .subject import Subject
-from .study_session import StudySession
-from .mood_log import MoodLog
--- planner/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import daily_checkin
-
-urlpatterns = [
-    path('checkin/', daily_checkin, name='daily_checkin'),
-]
